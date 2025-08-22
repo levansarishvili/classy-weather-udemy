eb3b6d004d6f90a7abd82c66ea1206285dad136d
Levani Sarishvili <levanisarishvili322@gmail.com>
2025-08-22 14:40:18 +0400

    feat: add lifecycle methods

--- src/App.js
+++ src/App.js
@@ -34,13 +34,15 @@ function formatDay(dateStr) {
 
 class App extends React.Component {
   state = {
-    location: "tbilisi",
+    location: "",
     isLoading: false,
     displayLocation: "",
     weather: {},
   };
 
   fetchWeather = async () => {
+    if (this.state.location.length < 2) return;
+
     try {
       this.setState({ isLoading: true });
       // 1) Getting location (geocoding)
@@ -65,25 +67,37 @@ class App extends React.Component {
       const weatherData = await weatherRes.json();
       this.setState({ weather: weatherData.daily });
     } catch (err) {
-      console.err(err);
+      console.error(err);
     } finally {
       this.setState({ isLoading: false });
     }
   };
 
+  setLocation = (e) => this.setState({ location: e.target.value });
+
+  componentDidMount() {
+    // this.fetchWeather();
+
+    this.setState({ location: localStorage.getItem("location") || "" });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.location !== prevState.location) {
+      this.fetchWeather();
+
+      localStorage.setItem("location", this.state.location);
+    }
+  }
+
   render() {
     return (
       <div className="app">
         <h1>Classy Weather</h1>
-        <div>
-          <input
-            type="text"
-            placeholder="Search for location..."
-            value={this.state.location}
-            onChange={(e) => this.setState({ location: e.target.value })}
-          ></input>
-        </div>
-        <button onClick={this.fetchWeather}>Get weather</button>
+
+        <Input
+          location={this.state.location}
+          onChangeLocation={this.setLocation}
+        />
 
         {this.state.isLoading && <p className="loader">Loading...</p>}
 
@@ -99,6 +113,21 @@ class App extends React.Component {
 }
 
 export default App;
+
+class Input extends React.Component {
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          placeholder="Search for location..."
+          value={this.props.location}
+          onChange={this.props.onChangeLocation}
+        ></input>
+      </div>
+    );
+  }
+}
 
 class Weather extends React.Component {
   render() {
